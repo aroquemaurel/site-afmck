0ff4ebb636f2c0498370e4343ecaa46d0c37e896
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2014-12-22 12:30:48 +0100

    Add hydratation

--- classes/DatabaseUser.php
+++ classes/DatabaseUser.php
@@ -2,25 +2,22 @@
 /**
  * Created by PhpStorm.
  * User: aroquemaurel
- * Date: 03/12/14
- * Time: 22:18
+ * Date: 22/12/14
+ * Time: 11:31
  */
 
 require_once('Database.php');
 
-class DatabaseConnection extends Database {
-    public function connect(User& $user) {
-        $adeliNumber = $user->getAdeliNumber();
-        $password = $user->getPassword();
+class DatabaseUser extends Database {
+    public function getUser($adeliNumber, $password) {
         $query = $this->dbAccess->prepare("SELECT adeliNumber from user WHERE adeliNumber = :number AND password=:password");
         $query->bindParam(":number", $adeliNumber, PDO::PARAM_INT);
         $query->bindParam(":password", $password, PDO::PARAM_STR);
         $query->execute();
 
-        return $query->fetchObject() != null;
-    }
-
-    public function disconnect() {
-        echo "TODO disconnection";
+        $data =  $query->fetchObject();
+        if($data == null) {
+            return false;
+        }
     }
 } 
