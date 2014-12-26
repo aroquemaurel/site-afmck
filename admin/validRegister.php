12a9b9869106d50925a061eb7af56a6b1cfa0f05
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2014-12-26 23:19:16 +0100

    Add database function for get users to valid

--- admin/validRegister.php
+++ admin/validRegister.php
@@ -5,6 +5,9 @@ use utils\Link;
 $title = 'Validation des incriptions';
 $breadcrumb = new utils\Breadcrumb(array(new Link('home', 'index.php'), new Link('Espace membres', '#'),
     new Link('Administration','#'), new Link('Validation des inscriptions', '#')));
+$db = new DatabaseUser();
+$users = $db->getUsersToValid();
+
 include('../views/includes/head.php');
 include('../views/admin/validRegister.php');
 include('../views/includes/foot.php');
