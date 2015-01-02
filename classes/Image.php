789824de86f83c900eeea188a8de650c5313dfdc
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-01-02 14:59:49 +0100

    Generation of pdf min

--- classes/Image.php
+++ classes/Image.php
@@ -8,10 +8,28 @@
 
 class Image {
     public static function miniLink($folder, $filename, $title) {
-        echo '<li><a target="'.$folder.'/'.$filename.'.jpg"
+        if(!file_exists($folder.'/mini/'.$filename.'.jpg')) {
+            self::createPdfMini($folder.'/'.$filename.'.pdf', $folder.'/mini/'.$filename.'.jpg');
+        }
+        echo '<li><a target="'.$folder.'/mini/'.$filename.'.jpg"
                 class="miniLink" name="'.$title.'"
                 href="'.$folder.'/'.$filename.'.pdf">
                 <i class="glyphicon glyphicon-download-alt"></i>
                 '.$title.'</a></li>';
+    }
+
+    public static function createPdfMini($in, $out) {
+        exec("/usr/bin/gs -o \"$out\" -sDEVICE=jpeg -r70 \"$in\"");
+    }
+
+    public static function miniLinkJs() {
+        return "<script>
+$('.miniLink').bind('mouseenter', function() {
+        img = $(this).attr('target');
+        $('#mini').attr('src', img);
+        $('#description').empty();
+        $('#description').append($(this).attr('name'));
+});
+</script>";
     }
 }
