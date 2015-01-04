8304d13f3abc21d54879161f41fd7133ba4704f7
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-01-04 22:33:52 +0100

    Correction of breadcrumb

--- admin/user.php
+++ admin/user.php
@@ -7,7 +7,8 @@ $user = $db->getUserById($_GET['id']);
 $title = 'Utilisateur '.$user->getFirstName()." ".$user->getLastName();
 
 $breadcrumb = new utils\Breadcrumb(array(new Link('home', 'index.php'), new Link('Espace membres', Visitor::getInstance()->getRootPage()."/members/index.php"),
-    new Link('Administration','#'), new Link('Membre '.$user->getFirstName()." ".$user->getLastName(),  '#')));
+    new Link('Administration','#'), new Link('Membres', VIsitor::getInstance()->getRootPage().'/admin/members.php'),
+    new Link($user->getFirstName()." ".$user->getLastName(),  '#')));
 
 include('../views/includes/head.php');
 include('../views/admin/user.php');
