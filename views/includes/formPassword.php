9e044c4076eb569ae6be495f26e7d360f662fdfe
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-01-04 16:02:21 +0100

    Add glyphicons, phones and address complement in register form

--- views/includes/formPassword.php
+++ views/includes/formPassword.php
@@ -1,9 +1,14 @@
 <div class="row">
     <div class="col-xs-12 col-sm-6 col-md-6">
         <div class="form-group">
+            <div class="input-group input-group-lg">
             <input required="required" type="password" name="password" id="password"
                    autocomplete="off" class="form-control input-lg"
                    placeholder="Mot de passe" tabindex="5">
+                                                                         <span class="input-group-addon" id="sizing-addon1">
+                                         <i class="glyphicon glyphicon-lock"></i></span>
+
+            </div>
         </div>
         <span id="8char" class="glyphicon glyphicon-remove" style="color:#FF0004;"></span> Longueur
         6 caractères<br>
@@ -12,10 +17,16 @@
     </div>
     <div class="col-xs-12 col-sm-6 col-md-6">
         <div class="form-group">
+            <div class="input-group input-group-lg">
+
             <input required="required" type="password" name="passwordConfirmation"
                    autocomplete="off" id="passwordConfirmation"
                    class="form-control input-lg" placeholder="Confirmation du mot de passe"
                    tabindex="6">
+                                                         <span class="input-group-addon" id="sizing-addon1">
+                                         <i class="glyphicon glyphicon-lock"></i></span>
+
+            </div>
         </div>
         <span id="pwmatch" class="glyphicon glyphicon-remove" style="color:#FF0004;"></span> Les
         deux mots de passes sont différents<br>
--- views/inscription.php
+++ views/inscription.php
@@ -5,8 +5,9 @@ if(!$editing) {
     echo '<h1>Formulaire d\'inscription</h1>';
     echo '<div class="col-xs-14 col-sm-10 col-md-11 col-sm-offset-1 col-md-offset-2">
             <div class="alert alert-warning">
-                <strong>Attention!</strong> Votre inscription n\'est possible que si vous êtes adhérent à l\'association. <br/>
-                Votre inscription sera validée manuellement par un membre du CA. Une fois validée, cette inscription sera valable pendant un an.
+                <strong>Attention!</strong>
+                Votre inscription sera validée manuellement par un membre du CA après réception de votre paiement.<br/>
+                Cette adhésion, et donc l\'accès au site, est valable une année.
             </div>';
     $user = new User();
 } else {
@@ -18,7 +19,7 @@ if(!$editing) {
             <form role="form" method="post">
 <?php
 if(!$editing) {
-    echo '<h2>Inscrivez-vous ! <small>Pour pouvoir profiter du site</small></h2>';
+    echo '<h2>Adhérez à l\'AFMcK! <small>Et profitez de tous ses avantages</small></h2>';
 }
 ?>
         <hr class="colorgraph">
@@ -46,49 +47,70 @@ if(!$editing) {
                    >
                    <span id="adeli" class="glyphicon glyphicon-remove" style="color:#FF0004;"></span> Numéro valide
         </div>
-        <div class="form-group">
-            <input required="required" type="email" name="email" id="email"
-                   class="form-control input-lg" placeholder="Adresse e-mail" tabindex="4"
-                   value="<?php echo $user->getMail();?>"
-                   >
-        </div>
-        <?php
-        if(!$editing) {
-            include('includes/formPassword.php');
-        }
-        ?>
-        <div class="row" style="margin-top: 15px;">
+        <fieldset>
+            <legend>Contact</legend>
+
+            <div class="form-group">
+            <div class="input-group input-group-lg">
+                <input required="required" type="email" name="email" id="email"
+                       class="form-control input-lg" placeholder="Adresse e-mail" tabindex="4"
+                       value="<?php echo $user->getMail();?>"
+                       >
+              <span class="input-group-addon" id="sizing-addon1">@</span>
+            </div>
+            </div>
+                    <div class="row">
             <div class="col-xs-12 col-sm-6 col-md-6">
                 <div class="form-group">
-                    <select tabindex="7" class="form-control input-lg" required="required" id="levelFormation" name="levelFormation">
-                        <option disabled selected>Niveau de formation MDT</option>
-                        <option <?php if($user->getLevelFormation() == 1) echo "selected"?> value="1">A</option>
-                        <option <?php if($user->getLevelFormation() == 2) echo "selected"?> value="2">B</option>
-                        <option <?php if($user->getLevelFormation() == 3) echo "selected"?> value="3">C</option>
-                        <option <?php if($user->getLevelFormation() == 4) echo "selected"?> value="4">D</option>
-                        <option <?php if($user->getLevelFormation() == 5) echo "selected"?> value="5">Certifié</option>
-                        <option <?php if($user->getLevelFormation() == 6) echo "selected"?> value="6">Diplômé</option>
-                    </select>
+                            <div class="input-group input-group-lg">
+                    <input required="required" type="text" name="cp" id="cp"
+                           class="form-control input-lg" placeholder="Téléphone professionnel" tabindex="10"
+                           value="<?php echo$user->getCp();?>"
+                           >
+                         <span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-earphone"></i></span>
+
+                </div>
                 </div>
             </div>
             <div class="col-xs-12 col-sm-6 col-md-6">
                 <div class="form-group">
-                    <div required="required" class='input-group date' id="formationDate">
-                        <input data-date-format="MM/YYYY" tabindex="8" type='text'  id='formationDate1' name='formationDate' placeholder="Date de validation" class="form-control input-lg"
-                        value="<?php echo ($user->getFormationDate() != NULL ? $user->getFormationDate()->format("m/Y") : "");?>"
-                        />
-                        <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span>
-                        </span>
-                    </div>
+                            <div class="input-group input-group-lg">
+                    <input required="required" type="text" name="town" id="town"
+                           class="form-control input-lg" placeholder="Téléphone mobile" tabindex="11"
+                           value="<?php echo $user->getTown();?>"
+                           >
+                                         <span class="input-group-addon" id="sizing-addon1">
+                                         <i class="glyphicon glyphicon-phone"></i></span>
+
+                           </div>
                 </div>
             </div>
         </div>
+
+        </fieldset>
+
+        <fieldset>
+        <legend>Adresse</legend>
         <div class="form-group">
+                            <div class="input-group input-group-lg">
             <input required="required" type="text" name="address" id="address"
-                   class="form-control input-lg" placeholder="Adresse" tabindex="9"
+                   class="form-control input-lg" placeholder="Adresse (Rue et numéro de rue)" tabindex="9"
                    value="<?php echo $user->getAddress();?>">
+                                         <span class="input-group-addon" id="sizing-addon1">
+                                         <i class="glyphicon glyphicon-envelope"></i></span>
+                   </div>
         </div>
-                        <div class="row">
+                <div class="form-group">
+                            <div class="input-group input-group-lg">
+            <input type="text" name="complementAddresse" id="complementAddresse"
+                   class="form-control input-lg" placeholder="Complément d'adresse (Bâtiment, Appartement, …)" tabindex="9"
+                   value="<?php echo $user->getAddress();?>">
+                                         <span class="input-group-addon" id="sizing-addon1">
+                                         <i class="glyphicon glyphicon-envelope"></i></span>
+                   </div>
+        </div>
+
+        <div class="row">
             <div class="col-xs-12 col-sm-6 col-md-6">
                 <div class="form-group">
                     <input required="required" type="text" name="cp" id="cp"
@@ -106,7 +128,51 @@ if(!$editing) {
                 </div>
             </div>
         </div>
+        </fieldset>
+        <fieldset>
+        <legend>Mot de passe</legend>
+        <?php
+        if(!$editing) {
+            include('includes/formPassword.php');
+        }
+        ?>
+        </fieldset>
 
+        <fieldset style="margin-top: 15px;">
+        <legend>Formation MDT</legend>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-6">
+                <div class="form-group">
+                    <select tabindex="7" class="form-control input-lg" required="required" id="levelFormation" name="levelFormation">
+                        <option disabled selected>Niveau de formation MDT</option>
+                        <option <?php if($user->getLevelFormation() == 1) echo "selected"?> value="1">A</option>
+                        <option <?php if($user->getLevelFormation() == 2) echo "selected"?> value="2">B</option>
+                        <option <?php if($user->getLevelFormation() == 3) echo "selected"?> value="3">C</option>
+                        <option <?php if($user->getLevelFormation() == 4) echo "selected"?> value="4">D</option>
+                        <option <?php if($user->getLevelFormation() == 5) echo "selected"?> value="5">Certifié</option>
+                        <option <?php if($user->getLevelFormation() == 6) echo "selected"?> value="6">Diplômé</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6">
+                <div class="form-group">
+                    <div required="required" class='input-group date input-group-lg' id="formationDate">
+                        <input data-date-format="MM/YYYY" tabindex="8" type='text'  id='formationDate1' name='formationDate' placeholder="Date de validation" class="form-control input-lg"
+                        value="<?php echo ($user->getFormationDate() != NULL ? $user->getFormationDate()->format("m/Y") : "");?>"
+                        />
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </fieldset>
+        <fieldset>
+            <legend>Newsletter</legend>
+            <div class="form-group">
+                    <input type="checkbox"> Je ne souhaite pas recevoir la newsletter
+            </div>
+        </fieldset>
         <hr class="colorgraph">
             <button id="submit" type="submit" style="margin: auto; width: 250px; "
             class="btn btn-primary btn-block btn-lg">
