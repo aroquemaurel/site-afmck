f7b15ebe5c65e5621fb3a49c701f2cc5a3341dd2
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-01-07 11:47:20 +0100

    Correction of home problem

--- views/index.php
+++ views/index.php
@@ -1,12 +1,11 @@
     <div class="container-fluid">
         <h1>Bienvenue sur le site de l'Association Française McKenzie !</h1>
 
-    <div id="facebookhome">
+        <div id="toc" class="toc">
         <div class="fb-like-box fb_iframe_widget" data-href="https://www.facebook.com/AFMcK" data-width="240" data-height="470" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true" fb-xfbml-state="rendered" fb-iframe-plugin-query="app_id=&amp;color_scheme=light&amp;header=true&amp;height=470&amp;href=https%3A%2F%2Fwww.facebook.com%2FAFMcK&amp;locale=fr_FR&amp;sdk=joey&amp;show_border=true&amp;show_faces=true&amp;stream=false&amp;width=240"><span style="vertical-align: bottom; width: 240px; height: 470px;"><iframe name="f1f68f2928" width="240px" height="470px" frameborder="0" allowtransparency="true" scrolling="no" title="fb:like_box Facebook Social Plugin" src="http://www.facebook.com/plugins/like_box.php?app_id=&amp;channel=http%3A%2F%2Fstatic.ak.facebook.com%2Fconnect%2Fxd_arbiter%2F7r8gQb8MIqE.js%3Fversion%3D41%23cb%3Df1596d1264%26domain%3Dafmck.fr%26origin%3Dhttp%253A%252F%252Fafmck.fr%252Ff37f26cd3c%26relation%3Dparent.parent&amp;color_scheme=light&amp;header=true&amp;height=470&amp;href=https%3A%2F%2Fwww.facebook.com%2FAFMcK&amp;locale=fr_FR&amp;sdk=joey&amp;show_border=true&amp;show_faces=true&amp;stream=false&amp;width=240" style="border: none; visibility: visible; width: 240px; height: 470px;" class=""></iframe></span></div>
     </div>
 
-
-            <?php include('views/includes/homeCarousel.php'); ?>
+        <?php include('views/includes/homeCarousel.php'); ?>
         <p class=""style="text-align: center;">
             <a href="docdwld/Presentation association AFMcK 2014_AFMcK.pdf" target="_blank">
                 <i class="glyphicon glyphicon-download-alt"></i>Télécharger le document de présentation
