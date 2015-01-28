5341a702c69851cd20ce6f90715ea10365dc3051
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-01-28 23:58:18 +0100

    Add doc and remove pgadh

--- pgadh/footervisit.php
+++
@@ -1,9 +0,0 @@
-<!-- TAG CRAWLPROTECT -->
-<?php require_once("/home/afmck/www/crawlprotect/include/cppf.php"); ?>
-
-<table class="footext">
-          <tr><td><h1>Nous contacter :</h1><a href="mailto:contact@afmck.fr"><h2>AFMcK</h2><p>2 rue Charles PIOT</p><p>38320 EYBENS</p><h3>contact@afmck.fr</h3></a></td>
-          <td><h1>Inscriptions :</h1><a href="mailto:tresorerie@afmck.fr"><h2>Anne-Marie GASTELLU-ETCHEGORRY</h2><p>27 av. du 10ème Dragon</p><p>82000 Montauban</p><h3>tresorerie@afmck.fr</h3></a></td>
-          <td class="facebook"><h1>...et retrouvez nous aussi sur facebook !</h1><a href="https://www.facebook.com/events/145851955619114/?ref=5#!/AFMcK" target="_BLANK" title="AFMcK"><img src="img/facebook_footer.jpg" width="100" height="30" style="border: 0px;" /></a></td>
-          </tr>
-        </table>
