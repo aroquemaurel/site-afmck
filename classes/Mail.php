b16208bd7be6479cdf0a9a1bbf630594b9065424
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-01-30 22:26:06 +0100

    Problems utf8 for password mail

--- classes/Mail.php
+++ classes/Mail.php
@@ -34,12 +34,12 @@ class Mail {
                     Le bureau de l'AFMcK";
     }
     public static function getForgetPassword($name, $param) {
-        return "Bonjour $name,<br/>
+        return utf8_decode("Bonjour $name,<br/>
                 Vous avez demandé un changement de mot de passe, vous pouvez changer de mot de passe à l'adresse suivante : <br/>
                 <a href=\"http://afmck.fr/setpassword.php?$param\">
                 http://afmck.fr/setpassword.php?$param
                 </a><br/><br/>
-                Le bureau de l'AFMcK";
+                Le bureau de l'AFMcK");
     }
 
     public static function getNewAccountTresor(User $user) {
