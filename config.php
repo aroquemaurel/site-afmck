7d061f13b7a8b75191637c34f1d83fc8703022c1
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-01-31 01:34:16 +0100

    Prod config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-define('ROOT_PAGE', '/dev/site-afmck');
-define('ROOT_PATH', '/data/dev/www');
+define('ROOT_PAGE', '/preprod');
+define('ROOT_PATH', '/homez.441/afmck/www/');
 define('TRESORERIE_MAIL', 'trash.dev.zero@gmail.com');
