702ceefaf26fd357c8b5266bc8d587c20e3255e4
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-02-20 14:57:51 +0100

    Lille days

--- views/members/travaux-association/journees/lille.php
+++ views/members/travaux-association/journees/lille.php
@@ -2,12 +2,14 @@
 <div class="container-fluid">
     <h1>4èmes Journées de l'AFMcK — Lille 2015</h1>
 </div><!--/.well -->
-<div style="text-align: center">
-    <a href="<?php echo Visitor::getInstance()->getRootPage() ?>/docs/members/afmckday/lyon/PRESENTATION_AFMcK.pdf" target="_blank">
-        <img src="<?php echo Visitor::getInstance()->getRootPage() ?>/docs/members/afmckday/lyon/PRESENTATION_AFMcK.jpg" width="450" height="349" alt="img presentation " />
-    </a>
-</div>
+
 
 <h2>Les Interventions</h2>
-
+<ul>
+    <li>
+<?php
+Image::modalIframe(Visitor::getInstance()->getRootPage()."/docs/members/afmckday/lille/DEVILLE/index.html", "Étude de Guillaume DEVILLE");
+?>
+    </li>
+</ul>
 </div>
