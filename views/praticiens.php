7211ecc13c0e6a8a355e64703f8f9d2131c944d0
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-03-05 12:57:26 +0100

    Add a little popup for data in maps

--- views/praticiens.php
+++ views/praticiens.php
@@ -16,15 +16,18 @@
     </div>
 
 <?php
-$arrayUsers = '[';
+$arrayAddress = '[';
+$arrayUser = '[';
 foreach($users as $user) {
     if ($user->getAddress() != "") {
-        $arrayUsers .= "\"".str_replace(',', '', $user->getAddress()." ".$user->getCp()." ".$user->getTown()) . '",';
+        $arrayAddress .= "\"".str_replace(',', '', $user->getAddress()." ".$user->getCp()." ".$user->getTown()) . '",';
+        $arrayUser .= "'<b>".$user->getFirstName()."</b>',";
     }
 }
-$arrayUsers = rtrim($arrayUsers, ",");
-$arrayUsers .= ']';
-echo $arrayUsers;
+$arrayUser = rtrim($arrayUser, ",");
+$arrayUser .= ']';
+$arrayAddress = rtrim($arrayAddress, ",");
+$arrayAddress .= ']';
     $script = '   <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>';
     $script .= "<script>
  $(document).ready(function () {
@@ -37,20 +40,35 @@ echo $arrayUsers;
     };
     map = new google.maps.Map($('#map-canvas')[0], myOptions);
 
-    var addresses = ".$arrayUsers.";
-
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
+    var addresses = ".$arrayAddress.";
+    var users = ".$arrayUser.";
+    var infoWindow = new google.maps.InfoWindow;
+    var markers = new Array();";
+   // for (var x = 0; x < addresses.length; x++) {
+   foreach($users as $user) {
+       if ($user->getAddress() != "") {
+           $script .= "$.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address=" . addslashes($user->getAddress()." ".$user->getCp()." ".$user->getTown()) . "&sensor=false', null, function (data) {
+            var p = data.results[0].geometry.location;
             var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map
-            });
 
-        });
-    }
-
+            addMarker(new google.maps.Marker({
+                position : latlng,
+				map : map,
+				content : '<b>".$user->getFirstName()."</b>'
+			}));
+        });";
+       }
+   }
+       $script .= "
+  //  }
+		function addMarker(marker){
+			google.maps.event.addListener(marker, 'click', function(){
+				var latLng = marker.getPosition();
+			    infoWindow.setContent(marker.content);
+			    infoWindow.open(map, marker);
+			});
+			markers.push(marker);
+		}
 });
   </script>;";
     ?>
