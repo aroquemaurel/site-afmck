67733fd4e8dd75b3e391de1a28c59ede27cd5aa3
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-03-05 13:31:58 +0100

    Add beautiful markers : markercluster

--- views/praticiens.php
+++ views/praticiens.php
@@ -29,38 +29,43 @@ $arrayUser .= ']';
 $arrayAddress = rtrim($arrayAddress, ",");
 $arrayAddress .= ']';
     $script = '   <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>';
+$script .='<script type="text/javascript" src="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclusterer/src/markerclusterer.js"></script>';
     $script .= "<script>
- $(document).ready(function () {
     var map;
     var elevator;
     var myOptions = {
-        zoom: 6,
+        zoom: 4,
         center: new google.maps.LatLng(46.5865209, 1.2814561),
-        mapTypeId: 'terrain'
+        	          mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     map = new google.maps.Map($('#map-canvas')[0], myOptions);
 
     var addresses = ".$arrayAddress.";
     var users = ".$arrayUser.";
     var infoWindow = new google.maps.InfoWindow;
-    var markers = new Array();";
+    var markers = new Array();
+    var markerCluster;
+    var thereisAjax = false;";
+
    // for (var x = 0; x < addresses.length; x++) {
    foreach($users as $user) {
        if ($user->getAddress() != "") {
-           $script .= "$.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address=" . addslashes($user->getAddress()." ".$user->getCp()." ".$user->getTown()) . "&sensor=false', null, function (data) {
+
+           $script .= "thereisAjax = true;$.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address=" . addslashes($user->getAddress()." ".$user->getCp()." ".$user->getTown()) . "&sensor=false', null, function (data) {
             var p = data.results[0].geometry.location;
             var latlng = new google.maps.LatLng(p.lat, p.lng);
-
+            thereisAjax = false;
             addMarker(new google.maps.Marker({
                 position : latlng,
 				map : map,
+				draggable: false,
 				content : '<b>".$user->getFirstName()."</b>'
 			}));
         });";
        }
    }
+
        $script .= "
-  //  }
 		function addMarker(marker){
 			google.maps.event.addListener(marker, 'click', function(){
 				var latLng = marker.getPosition();
@@ -69,6 +74,11 @@ $arrayAddress .= ']';
 			});
 			markers.push(marker);
 		}
-});
-  </script>;";
+
+        setTimeout(function (){
+            markerCluster = new MarkerClusterer(map, markers);
+        }, 500); // How long do you want the delay to be (in milliseconds)?
+
+
+</script>";
     ?>
