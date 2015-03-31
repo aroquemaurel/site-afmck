8b4c9f5f1d9aef997aae59acf9235ff022433cd0
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-03-31 21:36:21 +0200

    Spell corrections from Anne-Marie

--- views/members/travaux-association/cas-cliniques/epaule.php
+++ views/members/travaux-association/cas-cliniques/epaule.php
@@ -6,7 +6,7 @@
     <p>Par Romain-Brice LOMER</p>
 
     <h2>Interrogatoire</h2>
-    <p>Mr J 32 ans, chauffeur routier en reconversion professionnelle, vient consulter pour des douleurs à  l’épaule droite présentes depuis 2 mois qui se sont aggravées en 15 jours. Il est en arrêt de travail car il ne peut plus se servir de son épaule et a stoppé toute activité de loisirs. L’EVA est de 8/10 lors des mouvements et 3/10 au repos. Rien ne semble être à l’origine de la douleur qui progressivement s’est accentuée devenant constante au cours de la journée et l’empêchant de dormir, ne trouvant pas de position indolore.</p>
+    <p>M. J 32 ans, chauffeur routier en reconversion professionnelle, vient consulter pour des douleurs à  l’épaule droite présentes depuis 2 mois qui se sont aggravées en 15 jours. Il est en arrêt de travail car il ne peut plus se servir de son épaule et a stoppé toute activité de loisirs. L’EVA est de 8/10 lors des mouvements et 3/10 au repos. Rien ne semble être à l’origine de la douleur qui progressivement s’est accentuée devenant constante au cours de la journée et l’empêchant de dormir, ne trouvant pas de position indolore.</p>
     <p>La seule chose qui soulage l’épaule de Mr J c’est le repos strict, assis.</p>
     <p>La radiographie ne montre rien de particulier, la prise d’antalgique et d’AINS semble soulager quelques heures. Pour pouvoir dormir le patient prend des somnifères. Aucun drapeau rouge n’est présent.</p>
 
@@ -25,11 +25,11 @@
     <p>Amplitudes de l’épaule inchangées, mais des douleurs au repos qui par intermittence disparaissent. Les amplitudes de rétraction, extension  ont augmenté, mais pas de modifications pour l’inclinaison et la rotation. Durant la séance, augmentation de la force par des rétractions/extension et des surpressions du patient en fin de la séance. On obtient une modification des amplitudes dans le plan sagittal et frontal.  On revoit aussi la posture assise.</p>
 
     <h2>Séance N°3 :</h2>
-    <p>Les douleurs au repos sont quasi nulles, mais toujours des douleurs la nuit lors des changements de positions. Les amplitudes articulaires de l’épaule ont augmentés : 50° de flexion, d’abduction, mais la rotation externe est toujours impossible. Au niveau cervical on note  une douleur de fin d’amplitude avec un craquement lors de la rétraction/extension. La rotation et l’inclinaison droite sont améliorées.</p>
+    <p>Les douleurs au repos sont quasi nulles, mais toujours des douleurs la nuit lors des changements de positions. Les amplitudes articulaires de l’épaule ont augmenté : 50° de flexion, d’abduction, mais la rotation externe est toujours impossible. Au niveau cervical on note  une douleur de fin d’amplitude avec un craquement lors de la rétraction/extension. La rotation et l’inclinaison droite sont améliorées.</p>
     <p>Traitement durant la séance par des surpressions du thérapeute en rétraction. Ce qui permet un gain complet en rotation et inclinaison et une augmentation à 90° de flexion et l’abduction reste à 50°.</p>
 
     <h2>Séance N°4 :</h2
-    <p>Le patient a pris un analgésique puissant qui lui a permis de ne plus avoir de douleur lors des changements de position  la nuit. Actuellement les mouvements de l’épaule sont indolores mais restent limités à 90° de flexion (actif ou passif) 50°d’ abduction active et 90° en passif. La rotation externe, n’a pas évoluée et reste impossible. </p>
+    <p>Le patient a pris un analgésique puissant qui lui a permis de ne plus avoir de douleur lors des changements de position  la nuit. Actuellement les mouvements de l’épaule sont indolores mais restent limités à 90° de flexion (actif ou passif) 50°d’ abduction active et 90° en passif. La rotation externe, n’a pas évolué et reste impossible. </p>
     <p>Les amplitudes cervicales sont complètes  et indolores.</p>
     <p>Au niveau du traitement, des adductions horizontales sont réalisées permettant une augmentation significative de la flexion passant à 120° et abduction à 90° en actif.</p>
 
@@ -38,7 +38,7 @@
     <p>La surpression du patient en Adduction horizontale ne permet pas de progression, mais les surpressions en flexion permettent un gain non maintenu de flexion et de rotation externe.</p>
 
     <h2>Séance N°6 :</h2>
-    <p>Grace à la surpression en flexion on obtient une flexion complète, une augmentation légère de la rotation externe mais une abduction qui n’a pas augmentée.</p>
+    <p>Grace à la surpression en flexion on obtient une flexion complète, une augmentation légère de la rotation externe mais une abduction qui n’a pas augmenté.</p>
 
     <h2>Évolution du traitement</h2>
     <p>Durant les  5 séances suivantes, les évolutions articulaires ont été lentes. Un retour à des mouvements répétés en adductions avec surpression en alternance avec des surpressions en flexion ont été nécessaires. </p>
