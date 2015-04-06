af1f8e201377d6fb2d0188ef03291e3ef9e996c8
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-04-06 23:22:12 +0200

    Update news

--- views/members/index.php
+++ views/members/index.php
@@ -4,6 +4,17 @@
     <h1>Bienvenue sur le site de l'Association Française McKenzie</h1>
     <div id="news" class="hidden-xs hidden-md thumbnail with-caption news" style="margin-right: -10px;margin-top: -80px; width: 220px">
         <h2>Actualités</h2>
+<h3>Lille 2015</h3>
+<h3><a href="<?php echo Visitor::getInstance()->getRootPage();?>/members/travaux-association/journees/lille.php">Les interventions de Lille sont en lignes&nbsp;!</a></h3>
+<hr/>
+<h3>Communication Interne</h3>
+<a href="<?php echo Visitor::getInstance()->getRootPage();?>/members/divers/com-interne.php">Assemblée Générale 2015</a>
+<hr/>
+<h3>Nouvelle traduction !</h3>
+<a href="http://afmck.fr/docs/members/kiosque/traduction/mini/KHAN_2009_Mecanotherapie.jpg">
+<img width="100" src="http://afmck.fr/docs/members/kiosque/traduction/mini/KHAN_2009_Mecanotherapie.jpg" /></a>
+Mécanothérapie. <small>Khan, Scott</small>
+<hr/>
 <a href="<?php echo Visitor::getInstance()->getRootPage();?>/docs/members/kiosque/fr/OTERO 2014 - Lombalgie prévalence des syndromes McKenzie - Kine la revue n 145.pdf"><img src="<?php echo Visitor::getInstance()->getRootPage();?>/docs/members/kiosque/fr/mini/OTERO 2014 - Lombalgie prévalence des syndromes McKenzie - Kine la revue n 145.jpg" width="100" /></a>
 Lombalgie prévalence des syndromes McKenzie – Jacky OTERO<hr/>
 <a href="<?php echo Visitor::getInstance()->getRootPage();?>/docs/members/kiosque/newsletters/MDT Newsletter_Vol 3 No 4-gb_AFMcK.pdf">
