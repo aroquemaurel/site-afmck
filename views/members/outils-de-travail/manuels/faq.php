e81acd91ab85a3e5951c2bdeec3aabe0d268f815
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-04-30 21:39:27 +0200

    Add questions for faq

--- views/members/outils-de-travail/manuels/faq.php
+++ views/members/outils-de-travail/manuels/faq.php
@@ -2,18 +2,46 @@
 <div class="container-fluid">
     <h1>Manuel : Foire aux Questions</h1>
     <div id="toc" class="toc"></div><!--/.well -->
-<!--
-Je souhaite adhérer
+    <h1>L'espace adhérents</h1>
+    <div class="alert alert-info" style="margin-top: 50px">
+        <p>Je souhaite changer mon mot de passe, c'est possible ?</p>
+    </div>
+    <p>Il est tout à fait possible de changer votre mot de passe, pour cela, vous devez cliquer sur votre nom en haut à droite puis ensuite
+    aller sur « Changer mon mot de passe »</p>
+    <p>Votre mot de passe doit respecter les même critères que précédemment, c'est-à-dire au moins 8 caractères et une majuscule.</p>
 
-Je veux changer mon mot de passe
-je veux changer mes informations
+    <div class="alert alert-info" style="margin-top: 50px">
+        <p>Comment puis-je changer mes informations personnelles renseignées lors de l'inscription ? </p>
+    </div>
 
-Je souhaite avoir ma licence pour le logiciel de bilan
-Je souhaite renouveller ma licence pour le logiciel de bilan
+    <div class="alert alert-info" style="margin-top: 50px">
+        <p>J'ai perdu mon mot de passe, comment je fais ?</p>
+    </div>
+    <h1>La charte de bonnes pratiques</h1>
+    <div class="alert alert-info" style="margin-top: 50px">
+        <p>Comment puis-je signer la charte de bonnes pratiques ? </p>
+    </div>
+    <div class="alert alert-info" style="margin-top: 50px">
+        <p>Comment faire pour apparaître sur la carte des praticiens ?</p>
+    </div>
+    <div class="alert alert-info" style="margin-top: 50px">
+        <p>Je ne comprends pas, j'ai signé la charte, mais je n'apparaît pas sur la carte des praticiens…</p>
+    </div>
 
+    <div class="alert alert-info" style="margin-top: 50px">
+        <p>Comment puis-je avoir une licence pour utilier le logiciel de bilan ? </p>
+    </div>
 
+    <h1>Le logiciel de Bilan : BilanMDT</h1>
+    <div class="alert alert-info" style="margin-top: 50px">
+        <p>Comment puis-je avoir une licence pour utilier le logiciel de bilan ? </p>
+    </div>
+    <div class="alert alert-info" style="margin-top: 50px">
+        <p>J'ai eu un message pour me dire que ma licence arrive à sa fin</p>
+    </div>
 
-Je souhaite signer la charte
-Je veux n'apparaît pas sur la carte des praticiens
--->
+    <div class="alert alert-info" style="margin-top: 50px">
+        <p>Puis-je utiliser le logiciel sans demander de licence ?</p>
+    </div>
+
 </div>
