9a78b8f6e8863970dd2ea168b7822f208ce634cc
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-05-03 18:52:26 +0200

    Finalization of comment-sinscire

--- views/AFMcK/comment-sinscire.php
+++ views/AFMcK/comment-sinscire.php
@@ -4,29 +4,48 @@
     <div id="toc" class="toc"></div><!--/.well -->
     <h2>Les conditions d'adhésions</h2>
     <p>
-        Avant de pouvoir demander à adhérer à l'Association Française McKenzie, vous devez tout d'abord vérifier que vous respecter les critères.
+        Avant de pouvoir demander à adhérer à l'Association Française McKenzie, vous devez tout d'abord vérifier le respects des critères d'adhésions. 
     </p><p>
 
-        Pour cela, vous pouvez vous rendre à la <a href="<?php echo Visitor::getInstance()->getRootPage();?>/AFMcK/adhesion.php">page suivante</a>.
+        Pour cela, vous pouvez vous rendre sur la <a href="<?php echo Visitor::getInstance()->getRootPage();?>/AFMcK/adhesion.php">page suivante</a>.
     </p>
     <h2>Remplir le formulaire</h2>
         <p>
-            Si vous remplissez les conditions d'adhésion à l'association, vous devez remplir le formulaire d'inscription en ligne.<br/>
+            Si vous remplissez les conditions d'adhésion à l'association, il faut remplir le formulaire d'inscription en ligne.<br/>
             Ce formulaire est assez simple à remplir.
         </p>
     <h3>Les informations personnelles</h3>
+<p>Comme le montre la figure ci-dessous, vous devez remplir les informations suivantes : </p>
+<ul>
+<li>Nom</li>
+<li>Prénom</li>
+<li>Numéro ADELI<small>Pour les étrangers, merci de contacter l'association via contact@afmck.fr</small></li>
+</ul>
     <p><img src="docs/AFMcK/AFMcK/1_1.png" />&nbsp;<img src="docs/AFMcK/AFMcK/1_2.png" /></p>
+	
     <h3>Le mot choix du mot de passe</h3>
-    <!-- TODO password -->
+<p>C'est-à-vous de choisir votre mot de passe. Celui-ci doit être composé de : 
+<ul>
+<li>Au moins 6 caractères</li>
+<li>Comporter au moins une majuscule</li>
+<li>Idéalement, au moins un chiffre</li>
+</ul>
+</p>
     <p><img src="docs/AFMcK/AFMcK/1_3.png" />&nbsp;<img src="docs/AFMcK/AFMcK/1_4.png" /></p>
     <h3>Les autres champs : certification, cotisation, …</h3>
-    <!-- TODO others fields -->
+	<p>Il est maintenant nécessaire de renseigner les autres champs nécessaires à l'association
+<ul>
+<li>Niveau de certification ainsi que le mois et l'année d'obtention au format MM/yyyy.</li>
+<li>Le montant de votre cotisation</li>
+<li>Si vous ne souhaitez pas recevoir la newsletter, vous pouvez cocher la case correspondante</li>
+</ul>
+</p>
     <p><img src="docs/AFMcK/AFMcK/1_5.png" /></p>
 
     <h2>Valider son adresse e-mail</h2>
     <p>
-        Une fois que vous avez validé le formulaire vous allez recevoir un e-mail à l'adresse que vous avez fourni.
-        Cet email doit arriver assez rapidement dans votre boite, si tel n'est pas le cas, vérifiez votre dossier de spam.
+        Une fois que le formulaire validé, vous allez recevoir un e-mail à l'adresse fournie. <br/>
+        Cet email doit arriver assez rapidement dans votre boite, si tel n'est pas le cas, vérifiez votre dossier de spam. Dans le cas contraire, vous pouvez envoyer un e-mail à maintenance@afmck.fr
     </p>
 
     <p>Ce courrier contient un lien permettant d'activer votre demande d'adhésion, il vous suffit de cliquer sur le lien.</p>
@@ -38,7 +57,7 @@
     <p>Une fois votre adresse email validée, vous allez recevoir un email contenant de plus amples informations. Cet email
     doit contenir en pièce jointe une fiche d'adhésion pré-remplie, cette fiche doit être retournée signée à la trésorerie</p>
     <div class="alert alert-info"><p>Vous pouvez envoyer cette fiche signée par mail à l'adresse tresorerie@afmck.fr ou par
-        courrier à l'adresse  Anne-Marie GASTELLU-ETCHEGORRY 27 av. du 10ème Dragon 82000 Montauban</p></div>
+        courrier à l'adresse  Anne-Marie GASTELLU-ETCHEGORRY 27 av. du 10ème Dragon 82000 Montauban. Il est possible de l'envoyer scannée.</p></div>
     <p><img src="docs/AFMcK/AFMcK/3.png" /></p>
     <h3>Le paiement</h3>
     <p>Vous pouvez payer par chèque ou par virement bancaire. Si vous avez choisi de payer par virement bancaire, vous devriez avoir reçu
@@ -51,12 +70,12 @@
     </div>
 
     <h2>Attendre le mail de validation</h2>
-    <p>Une fois que vous avez transmi votre paiement et votre fiche d'adhésion à la trésorerie, il faut attendre que votre
+    <p>Une fois que vous votre paiement transmi et votre fiche d'adhésion à la trésorerie, il faut attendre que votre
     compte soit validé. Celui-ci ne sera validé manuellement qu'au réception du paiement</p>
     <div class="bs-callout bs-callout-info"><p>Vous allez recevoir un e-mail pou vous informer que votre compte a été validé</p></div>
     <p><img src="docs/AFMcK/AFMcK/4.png" /></p>
     <h2>Vous pouvez maintenant vous connecter…</h2>
     <p>Votre compte a été activé ? Vous pouvez maintenant vous connecter au site avec comme identifiant votre numéro ADELI et comme mot de passe
-    celui que vous avez renseigné lors de l'étape 1.</p>
+    celui que vous avez renseigné lors de l'étape 1. Si vous avez oublié votre mot de passe, vous pouvez utiliser le lien « mot de passe oulié ».</p>
     <p><img src="docs/AFMcK/AFMcK/5.png" /></p>
 </div>
