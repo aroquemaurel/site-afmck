38cf46c26be7e605633886aaf9d130fa60f8ce1d
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2016-02-28 00:51:12 +0100

    Beginning of file uploading

--- views/admin/add-news.php
+++ views/admin/add-news.php
@@ -32,6 +32,11 @@
                 echo '<input type="hidden" name="id" value="'.$news->getId().'"/>';
             }
             ?>
+            <h2>Multiple Image Upload Form</h2>
+            <form enctype="multipart/form-data" action="" method="post">
+                First Field is Compulsory. Only JPEG,PNG,JPG Type Image Uploaded. Image Size Should Be Less Than 100KB.
+                <div id="filediv"><input name="file[]" type="file" id="file"/></div>
+                <input type="button" id="add_more" class="upload btn btn-info btn-xs" value="Add More Files"/>
             <button id="submit" type="submit" style="margin: auto; width: 250px; "
                     class="btn btn-primary btn-block btn-lg">
                 <i class="glyphicon glyphicon-ok-sign"></i>
@@ -53,4 +58,50 @@ $script .= "
             $('#hiddeninput').val(mysave);
         });
     });
+
+
+var abc = 0;      // Declaring and defining global increment variable.
+//  To add new input file field dynamically, on click of Add More Files button below function will be executed.
+$('#add_more').click(function() {
+$(this).before($(\"<div/>\", {
+id: 'filediv'
+}).fadeIn('slow').append($(\"<input/>\", {
+name: 'file[]',
+type: 'file',
+id: 'file'
+}), $(\"<br/><br/>\")));
+});
+// Following function will executes on change event of file input to select different file.
+$('body').on('change', '#file', function() {
+if (this.files && this.files[0]) {
+abc += 1; // Incrementing global variable by 1.
+var z = abc - 1;
+var x = $(this).parent().find('#previewimg' + z).remove();
+$(this).before(\"<div id='abcd\" + abc + \"' class='abcd'><img id='previewimg\" + abc + \"' src=''/></div>\");
+var reader = new FileReader();
+reader.onload = imageIsLoaded;
+reader.readAsDataURL(this.files[0]);
+$(this).hide();
+$(\"#abcd\" + abc).append($(\"<img/>\", {
+id: 'img',
+src: '".Visitor::getInstance()->getRootPage()."/style/img/x.png"."',
+alt: 'delete'
+}).click(function() {
+    $(this).parent().parent().remove();
+}));
+}
+});
+// To Preview Image
+function imageIsLoaded(e) {
+    $('#previewimg' + abc).attr('src', e.target.result);
+};
+$('#upload').click(function(e) {
+    var name = $(\":file\").val();
+    if (!name) {
+        alert(\"First Image Must Be Selected\");
+        e.preventDefault();
+    }
+});
+
 </script>";
+
