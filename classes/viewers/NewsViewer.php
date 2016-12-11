624d1e78812ff1f373997e6ffe51827095626fc2
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2016-12-11 23:56:32 +0100

    Add attachment in news admin list #74

--- classes/viewers/NewsViewer.php
+++ classes/viewers/NewsViewer.php
@@ -19,7 +19,7 @@ class NewsViewer
 
     }
 
-    public static function getHtmlNew(News $new) {
+    public static function getHtmlNew(News $new) : string {
         $ret = '<div style="padding-right: 80px">';
         $ret .= '<h2>' . $new->getTitle() . '&nbsp;<small>' . $new->getSubtitle() . '</small></h2>';
         $ret .= '<p style="margin-top: -8px;margin-bottom:15px"><small>Posté le ' . $new->getDate()->format('d / m / Y à H:i') . ' par ' . $new->getAuthor()->getFirstName() . ' ' . $new->getAuthor()->getLastname() . '</small></p>';
@@ -29,16 +29,20 @@ class NewsViewer
         if (count($new->getAttachments()) > 0) {
             $ret .= '<br/><div style="font-size:9.5pt">';
             $ret .= '<h5>Pièces jointes</h5>';
-            $ret .= '<ul>';
-            foreach ($new->getAttachments() as $att) {
-                $ret .= '<li><i class="glyphicon glyphicon-download-alt"></i>&nbsp;<a href="' . Visitor::getInstance()->getRootPage() . '/docs/members/news/' . $att->getPath() . '">' . $att->getTitle() . '</a></li>';
-            }
-            $ret .= '</ul></div>';
+            $ret .= self::getHtmlAttachmentsNews($new);
+            $ret .= '</div>';
         }
 
         return $ret;
     }
-
+    private static function getHtmlAttachmentsNews(News $new) : string {
+        $ret = '<ul>';
+        foreach ($new->getAttachments() as $att) {
+            $ret .= '<li><i class="glyphicon glyphicon-download-alt"></i>&nbsp;<a href="' . Visitor::getInstance()->getRootPage() . '/docs/members/news/' . $att->getPath() . '">' . $att->getTitle() . '</a></li>';
+        }
+        $ret .= '</ul>';
+        return $ret;
+    }
     public static function getHtmlNewsContents($news, int $page, int $nbPages) : string {
         $ret = '<div style="margin-bottom: 0px;">';
         (new Pagination($page, $nbPages, Visitor::getInstance()->getRootPage().'/members/index.php'))->toString();
@@ -54,32 +58,34 @@ class NewsViewer
 
     private static function getHtmlNewsListLine(News $new) {
         $ret = '';
-        $ret .= '<tr href="'.Visitor::getInstance()->getRootPage().'/admin/user.php?id='.$new->getId().'">'.
-            '<td>'.$new->getTitle().' <small>'.$new->getSubtitle().'</small></td>'.
-            '<td><i class="glyphicon glyphicon-user"></i> '.($new->getAuthor()->getFirstname().' '.$new->getAuthor()->getLastname()).'</td>'.
-            '<td>'.($new->getDate()->format('d/m/Y à H:i')).'</td>'.
-            '<td>'.(
+        $ret .= '<tr href="'.Visitor::getInstance()->getRootPage().'/admin/user.php?id='.$new->getId().'">';
+        $ret .=     '<td>'.$new->getTitle().' <small>'.$new->getSubtitle().'</small></td>';
+        $ret .= '<td><i class="glyphicon glyphicon-user"></i> '.($new->getAuthor()->getFirstname().' '.$new->getAuthor()->getLastname()).'</td>';
+        $ret .= '<td>'.($new->getDate()->format('d/m/Y à H:i')).'</td>';
+        $ret .= '<td class="attachment">'.self::getHtmlAttachmentsNews($new).'</td>';
+        $ret .= '<td>'.(
             !$new->isSend() ? ('<a href="'.Visitor::getInstance()->getRootPage().'/admin/envoyer-news.php?id='.$new->getId().'">
             <i class="glyphicon glyphicon-envelope"></i></a>')
                 : '<i class="glyphicon glyphicon-envelope"></i>'
-            ).
-            '&nbsp;&nbsp;<a href="'.Visitor::getInstance()->getRootPage().'/admin/editer-news.php?id='.$new->getId().'">
-            <i class="glyphicon glyphicon-pencil"></i></a>&nbsp;&nbsp;
-            <a href="'.Visitor::getInstance()->getRootPage().'/admin/supprimer-news.php?id='.$new->getId().'">
-            <i class="glyphicon glyphicon-trash"></i></a>
-            </td>';
+            );
+        $ret .= '&nbsp;&nbsp;<a href="'.Visitor::getInstance()->getRootPage().'/admin/editer-news.php?id='.$new->getId().'">';
+        $ret .= '<i class="glyphicon glyphicon-pencil"></i></a>&nbsp;&nbsp;';
+        $ret .= '<a href="'.Visitor::getInstance()->getRootPage().'/admin/supprimer-news.php?id='.$new->getId().'">';
+        $ret .= '<i class="glyphicon glyphicon-trash"></i></a>';
+        $ret .= '</td>';
         $ret .= '</tr>';
         return $ret;
     }
 
     public static function getHtmlNewsList($news, $page, $nbPages) {
         $ret = (new Pagination($page, $nbPages, Visitor::getInstance()->getRootPage().'/admin/list-news.php'))->toString();
-        $ret .= '<table class="table table-striped">';
+        $ret .= '<table class="table table-striped news-list">';
         $ret .= '<thead>';
         $ret .= '<tr>';
         $ret .= '<th>Titre</th>';
         $ret .= '<th>Auteur</th>';
         $ret .= '<th>Date</th>';
+        $ret .= '<th>Pièces Jointes</th>';
         $ret .= '<th>Actions</th>';
         $ret .= '</tr>';
         $ret .= '</thead>';
